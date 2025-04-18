b0f4f0ae304908ef94c11b151ebb40785582f46d
Mohammad Hasan <mohammad_hasan27@outlook.com>
2025-04-18 11:41:36 -0800

    add ArrayStack

---
+++ ArrayStack.java
@@ -0,0 +1,86 @@
+class ArrayStack<T> {
+    private int capacity;
+    private int top;
+    private T[] array;
+    private int size;
+    ArrayStack() {
+        this(14);
+    }
+
+    @SuppressWarnings("unchecked")
+    ArrayStack(int initialCapacity) {
+        if (initialCapacity < 1) {
+            throw new IllegalArgumentException("Invalid Stack Capacity: " + initialCapacity);
+        } else {
+            array = (T[])new Object[initialCapacity];
+            capacity = initialCapacity;
+            top = -1;
+            size = 0;
+        }
+    }
+
+    int capacity() {
+        return capacity;
+    }
+
+    int size() {
+        return size;
+    }
+
+    public String toString() {
+        String s = "[";
+        for (int i = 0; i < size; i++) {
+            s = s.concat(String.valueOf(array[i]));
+            if (i < size - 1) {
+                s = s.concat(", ");
+            }
+        }
+        s = s.concat("]");
+        return s;
+    }
+
+    boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean isFull() {
+        if (size == capacity) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    void push(T item) {
+        if (isFull()) {
+            throw new RuntimeException("Stack Overflow");
+        } else {
+            top = top + 1;
+            array[top] = item;
+            size = size + 1;
+        }
+    }
+
+    T pop() {
+        if (isEmpty()) {
+            throw new java.util.EmptyStackException();
+        } else {
+            T item = array[top];
+            top = top - 1;
+            size = size - 1;
+            return item;
+        }
+    }
+
+    T peek() {
+        if (isEmpty()) {
+            throw new java.util.EmptyStackException();
+        } else {
+            return array[top];
+        }
+    }
+}
