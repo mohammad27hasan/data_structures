8e6c4fbe3fff77579242576b23272a7cd3aae1cb
Mohammad Hasan <mohammad_hasan27@outlook.com>
2025-04-18 19:49:11 -0800

    add LinkedStack

---
+++ LinkedStack.java
@@ -0,0 +1,73 @@
+class LinkedStack<T> {
+    private class Node {
+        private T data;
+        private Node next;
+    }
+
+    private Node head;
+    private int size;
+    LinkedStack() {
+        head = null;
+        size = 0;
+    }
+
+    int size() {
+        return size;
+    }
+
+    private Node reverse(Node head) {
+        Node prevPtr = null;
+        Node currPtr = head;
+        Node nextPtr = null;
+        while (currPtr != null) {
+            nextPtr = currPtr.next;
+            currPtr.next = prevPtr;
+            prevPtr = currPtr;
+            currPtr = nextPtr;            
+        }
+        return prevPtr;
+    }
+
+    public String toString() {
+        head = reverse(head);
+        Node iterator = head;
+        String s = "[";
+        for (int i = 0; iterator != null; i++) {
+            s = s.concat(String.valueOf(iterator.data));
+            if (i < size - 1) {
+                s = s.concat(", ");
+            }
+            iterator = iterator.next;
+        }
+        s = s.concat("]");
+        head = reverse(head);
+        return s;
+    }
+
+    void push(T item) {
+        Node newHead = new Node();
+        newHead.data = item;
+        newHead.next = head;
+        head = newHead;
+        size = size + 1;
+    }
+
+    T pop() {
+        if (head == null) {
+            throw new java.util.EmptyStackException();
+        } else {
+            T item = head.data;
+            head = head.next;
+            size = size - 1;
+            return item;
+        }
+    }
+
+    T peek() {
+        if (head == null) {
+            throw new java.util.EmptyStackException();
+        } else {
+            return head.data;
+        }
+    }
+}
