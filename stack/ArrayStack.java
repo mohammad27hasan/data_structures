87ef8c3b02f603c2f03f23f6d819690ac9fc478f
Mohammad Hasan <mohammad_hasan27@outlook.com>
2025-10-26 17:30:29 -0800

    add ArrayStack v1.0.2

---
+++ stack/ArrayStack.java
@@ -0,0 +1,60 @@
+package ds.stack;
+
+public class ArrayStack<T> {
+    private int top;
+    private java.util.ArrayList<T> array;
+    public ArrayStack() {
+        this(10);
+    }
+
+    public ArrayStack(int initialCapacity) {
+        if (initialCapacity < 1) {
+            throw new IllegalArgumentException("Initial capacity is less than 1");
+        }
+        array = new java.util.ArrayList<T>(initialCapacity);
+        top = -1;
+    }
+
+    public int size() {
+        return (top + 1);
+    }
+
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+
+    public void push(T item) {
+        if (item == null) {
+            throw new NullPointerException("The specified item is null");
+        } 
+        top++;
+        array.add(item);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new java.util.EmptyStackException();
+        }
+        return array.remove(top--);
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new java.util.EmptyStackException();
+        }
+        return array.get(top);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        int commaSize = size - 1;
+        for (int i = 0; i < size; i++) {
+            sb.append(array.get(i));
+            if (i < commaSize) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
