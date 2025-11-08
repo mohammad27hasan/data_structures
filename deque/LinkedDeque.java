be9917e536ca643304dad05fff78e71e5d902e0c
Mohammad Hasan <mohammad_hasan27@outlook.com>
2025-11-07 16:57:03 -0800

    add LinkedDeque v1.0.1

---
+++ deque/LinkedDeque.java
@@ -0,0 +1,113 @@
+package ds.deque;
+
+public class LinkedDeque<T> {
+    private class Node {
+        Node prev;
+        T data;
+        Node next;
+        Node(T val) {
+            prev = null;
+            data = val;
+            next = null;
+        }
+    }
+    private Node front;
+    private Node rear;
+    private int size;
+    public LinkedDeque() {
+        front = null;
+        rear = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return (front == null);
+    }
+
+    public void pushBack(T item) {
+        if (item == null) {
+            throw new NullPointerException("Specified item is null");
+        }
+        Node newNode = new Node(item);
+        if (isEmpty()) {
+            front = newNode;
+        } else {
+            newNode.prev = rear;
+            rear.next = newNode;
+        }
+        rear = newNode;
+        size++;
+    }
+
+    public void pushFront(T item) {
+        if (item == null) {
+            throw new NullPointerException("Specified item is null");
+        }
+        Node newNode = new Node(item);
+        if (isEmpty()) {
+            rear = newNode;
+        } else {
+            newNode.next = front;
+            front.prev = newNode;
+        }
+        front = newNode;
+        size++;
+    }
+
+    public T popBack() {
+        if (isEmpty()) {
+            throw new java.util.NoSuchElementException("Linked deque is empty");
+        }
+        T element = rear.data;
+        rear = rear.prev;
+        rear.next = null;
+        size--;
+        return element;
+    }
+
+    public T popFront() {
+        if (isEmpty()) {
+            throw new java.util.NoSuchElementException("Linked deque is empty");
+        }
+        T element = front.data;
+        front = front.next;
+        front.prev = null;
+        size--;
+        return element;
+    }
+
+    public T back() {
+        if (isEmpty()) {
+            throw new java.util.NoSuchElementException("Linked deque is empty");
+        }
+        return rear.data;
+    }
+
+    public T front() {
+        if (isEmpty()) {
+            throw new java.util.NoSuchElementException("Linked deque is empty");
+        }
+        return front.data;
+    }
+
+    public String toString() {
+        if (isEmpty()) {
+            return "[]";
+        }
+        StringBuilder sb = new StringBuilder("[");
+        Node node = front;
+        while (node != null) {
+            sb.append(node.data);
+            if (node.next != null) {
+                sb.append(", ");
+            }
+            node = node.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
