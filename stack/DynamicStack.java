129f3eeaeda470ea0204dc04a87832b1e5ef9b7b
Mohammad Hasan <mohammad_hasan27@outlook.com>
2025-12-13 16:51:19 -0800

    add DynamicStack v1.0.5

--- stack/DynamicStack.java
+++ stack/DynamicStack.java
@@ -1,24 +1,25 @@
 /*
+* Data structure: Stack
 * Abstract data type: Dynamic stack
-* Version: 1.0.4
+* Version: 1.0.5
 * Author: Mohammad Hasan
 */
 package ds.stack;
 
 public class DynamicStack<T> {
-    private int top;
     private java.util.ArrayList<T> array;
-
-    public DynamicStack() {
-        this(10);
-    }
+    private int top;
     
     public DynamicStack(int capacity) {
         if (capacity < 1) {
-            throw new IllegalArgumentException("Specified capacity is less than 1");
+            throw new IllegalArgumentException("Illegal Capacity: " + capacity);
         }
-        top = -1;
         array = new java.util.ArrayList<T>(capacity);
+        top = -1;
+    }
+
+    public DynamicStack() {
+        this(10);
     }
 
     public int size() {
@@ -29,12 +30,12 @@ public class DynamicStack<T> {
         return (top == -1);
     }
 
-    public void push(T item) {
-        if (item == null) {
-            throw new NullPointerException("Specified item is null");
+    public void push(T element) {
+        if (element == null) {
+            throw new NullPointerException("Element: " + element);
         }
         top++;
-        array.add(item);
+        array.add(element);
     }
 
     public T pop() {
